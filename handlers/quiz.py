27b532c687b8fca408b2dce6be9e1cf5e20eb99a
TumantaevBaiaman <tumantaev.baiaman.space@gmail.com>
2023-06-08 01:14:44 +0600

    fix

--- handlers/quiz.py
+++ handlers/quiz.py
@@ -2,16 +2,16 @@ quiz_test = [
     {
         "question": "Вопрос 53: Choose the correct option",
         "options": ["a) When we finish the painting, we'll have a cup of tea", "b)  When we've finished the painting, we'll have a cup of tea", "c)  When the painting finishes, we'll have a cup of tea"],
-        "correct_answer": "a"
+        "correct_answer": 0
     },
     {
         "question": "Вопрос 54: He told her that...",
         "options": ["a)  He would love her forever", "b) He loved her forever", "c) He is loving her forever"],
-        "correct_answer": "a"
+        "correct_answer": 0
     },
     {
         "question": "Вопрос 55: Choose the correct option",
         "options": ["a) She asked the shop assistant to have a refund", "b)  She asked the shop assistant to give a refund", "c)  She asked the shop assistant for a refund"],
-        "correct_answer": "c"
+        "correct_answer": 2
     },
 ]
--- handlers/user.py
+++ handlers/user.py
@@ -9,40 +9,63 @@ from aiogram.dispatcher.filters.state import State, StatesGroup
 from create_bot import dp, bot
 from aiogram.dispatcher.filters import Text
 from key.buttons import main_menu
-import handlers.quiz as quiz
+import handlers.quiz as quiz_test
 
 
 class FSMChatGPT(StatesGroup):
-    start = State()
     poll = State()
     question = 0
     ls = 0
+    result = 0
 
 
-async def start(message: types.Message):
-    await bot.send_message(message.from_user.id, 'Привет! Проверим твои уровен по англискому')
-    await FSMChatGPT.start.set()
+async def cm_start(message: types.Message, state: FSMContext):
+    await bot.send_message(message.chat.id, 'Привет! Проверим твой уровень по английскому.')
+    async with state.proxy() as data:
+        data['ls'] = 0
+        data['result'] = 0
+
+    await show_poll(message, state)
 
 
-async def cm_start(message: types.Message, state=FSMChatGPT):
-    state = FSMChatGPT()
-    poll = types.Poll(
-        type=types.PollType.REGULAR,
-        question=quiz.quiz_test[state.ls]["question"],
-        options=quiz.quiz_test[state.ls]["options"],
-    )
-    await bot.send_poll(message.chat.id, poll.question, poll.options)
-    await FSMChatGPT.poll.set()
+async def show_poll(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        ls = data['ls']
+
+    if ls < len(quiz_test.quiz_test):
+        question = quiz_test.quiz_test[ls]['question']
+        options = quiz_test.quiz_test[ls]['options']
+
+        poll_options = []
+        for i, option in enumerate(options):
+            button = types.InlineKeyboardButton(text=option, callback_data=f'answer_{i}')
+            poll_options.append([button])
+
+        reply_markup = types.InlineKeyboardMarkup(inline_keyboard=poll_options)
+
+        await bot.send_message(message.from_user.id, question, reply_markup=reply_markup)
+
+        async with state.proxy() as data:
+            data['ls'] += 1
+
+        await FSMChatGPT.poll.set()
+
+    else:
+        async with state.proxy() as data:
+            result = data['result']
+        await bot.send_message(message.from_user.id, f"Опрос завершен. {result}")
+        await state.finish()
 
 
-async def process_poll_answer(poll_answer: types.PollAnswer, state=FSMChatGPT):
-    user_id = poll_answer.user.id
-    selected_option = poll_answer.option_ids[0]
-    await bot.send_message(user_id, f"Вы выбрали опцию {selected_option}")
-    await state.start.set()
+async def process_poll_answer(callback_query: types.CallbackQuery, state: FSMContext):
+    async with state.proxy() as data:
+        ls = data['ls']-1
+        correct_answer = quiz_test.quiz_test[ls]['correct_answer']
+        if str(correct_answer) == callback_query.data.replace("answer_", ""):
+            data['result'] += 1
+    await show_poll(callback_query, state)
 
 
 def register_quiz(dp: Dispatcher):
-    dp.register_message_handler(start, commands=['start'])
-    dp.register_message_handler(cm_start, state=FSMChatGPT.start)
-    dp.register_message_handler(process_poll_answer, state=FSMChatGPT.poll)
+    dp.register_message_handler(cm_start, commands=['start'])
+    dp.register_callback_query_handler(process_poll_answer, state=FSMChatGPT.poll)
