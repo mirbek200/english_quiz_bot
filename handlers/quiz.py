c3f9cc614fe8527c2ddc9d2b8ae2c44595a7aa34
Your Name <you@example.com>
2023-06-08 13:56:19 +0600

    first commit

--- handlers/quiz.py
+++ handlers/quiz.py
@@ -1,10 +1,270 @@
 quiz_test = [
     {
+        "question": "Вопрос 1: Do you work here? ",
+        "options": ["a) Yes, I am", "b) No, I’m not ", "c) No, I don’t", "d) Yes, they do"],
+        "correct_answer": 2
+    },
+    {
+        "question": "Вопрос 2: Who do you ____ with?",
+        "options": ["a) Working", "b) Worked", "c) Will work ", "d) Work"],
+        "correct_answer": 3
+    },
+    {
+        "question": "Вопрос 3: ____ any shops near here?",
+        "options": ["a) Is there", "b) Are there", "c) Is here", "d) Are here"],
+        "correct_answer": 1
+    },
+    {
+        "question": "Вопрос 4: We live in ____ USA",
+        "options": ["a) The", "b) A", "c) At", "d) –"],
+        "correct_answer": 0
+    },
+    {
+        "question": "Вопрос 5: ____ you ride a bicycle, when you were 10?",
+        "options": ["a) Can", "b) Will", "c) Do", "d) Could"],
+        "correct_answer": 3
+    },
+    {
+        "question": "Вопрос 6: I am ______ a shower at the moment",
+        "options": ["a) Have", "b) Will have", "c) Had", "d) Having"],
+        "correct_answer": 3
+    },
+    {
+        "question": "Вопрос 7: We ____ in a restaurant yesterday",
+        "options": ["a) Were", "b) Are", "c) Will go", "d) Gone"],
+        "correct_answer": 0
+    },
+    {
+        "question": "Вопрос 8: We ____ a lot of exercises last month",
+        "options": ["a) Done", "b) Did", "c) Will do", "d) Doing"],
+        "correct_answer": 1
+    },
+    {
+        "question": "Вопрос 9: I don’t _____ do this job",
+        "options": ["a) Want", "b) Feel", "c) Have to", "d) Need"],
+        "correct_answer": 2
+    },
+    {
+        "question": "Вопрос 10: Could you bring me ____ water, please?",
+        "options": ["a) a", "b) the", "c) -", "d) Some"],
+        "correct_answer": 3
+    },
+    {
+        "question": "Вопрос 11: Сhoose the correct option",
+        "options": ["a) Where are you from?", "b) What is your from?", "c) What from are you?"],
+        "correct_answer": 0
+    },
+    {
+        "question": "Вопрос 12: Are you French?",
+        "options": ["a) Yes, I is", "b) Yes, he is", "c) Yes, I am"],
+        "correct_answer": 2
+    },
+{
+        "question": "Вопрос 13: Сhoose the correct option",
+        "options": ["a) He live on London", "b) He live in London", "c) He lives in London"],
+        "correct_answer": 2
+    },
+    {
+        "question": "Вопрос 14: Сhoose the correct option",
+        "options": ["a) I don't like coffee", "b) I doesn't like coffee", "c) I does like coffee"],
+        "correct_answer": 0
+    },
+    {
+        "question": "Вопрос 15: Сhoose the correct option",
+        "options": ["a) He goes to work in bus", "b) He goes to work by bus", "c) He goes to work by the bus"],
+        "correct_answer": 1
+    },
+    {
+        "question": "Вопрос 16: Сhoose the correct option",
+        "options": ["a) George has very big car", "b) George a very big car has", "c) George has a very big car"],
+        "correct_answer": 2
+    },
+    {
+        "question": "Вопрос 17: Do you speak English?",
+        "options": ["a) Yes, we do", "b) Yes, we don't", "c) Yes, they do"],
+        "correct_answer": 0
+    },
+    {
+        "question": "Вопрос 18:  Сhoose the correct option",
+        "options": ["a) Look at that house over here", "b) Look at that house over there", "c) Look at that house under there"],
+        "correct_answer": 1
+    },
+    {
+        "question": "Вопрос 19: Сhoose the correct option",
+        "options": ["a) He Italian, he not Polish, I am", "b) He is Italian, he isn't Polish", "c) They is Italian, they isn't Polish"],
+        "correct_answer": 1
+    },
+    {
+        "question": "Вопрос 20: Сhoose the correct option",
+        "options": ["a) What do you have breakfast?", "b) When do you have breakfast?", "c) Where breakfast you?"],
+        "correct_answer": 1
+    },
+    {
+        "question": "Вопрос 21: The blue jacket costs $50, the white jacket costs $70. The blue jacket is …… than the white one.",
+        "options": ["a) Cheap", "b) Cheaper", "c) Less cheaper"],
+        "correct_answer": 1
+    },
+    {
+        "question": "Вопрос 22: I’d  rather go out for dinner tonight than …… dinner at home.",
+        "options": ["a) Have", "b) Having", "c) To have"],
+        "correct_answer": 0
+    },
+    {
+        "question": "Вопрос 23: My cousin …… his holidays with us next summer",
+        "options": ["a) Spends", "b) Is spending", "c) Was spending"],
+        "correct_answer": 1
+    },
+    {
+        "question": "Вопрос 24: By this time next year, Jane …… Spanish for two years.",
+        "options": ["a)  is going to study", "b) will be studying", "c) will have been studying"],
+        "correct_answer": 2
+    },
+    {
+        "question": "Вопрос 25: Let’s go shopping, ……?",
+        "options": ["a) let’s we", "b) shall we", "c) Do we"],
+        "correct_answer": 1
+    },
+    {
+        "question": "Вопрос 26: Alice won’t be able to buy that car …… she saves some money.",
+        "options": ["a) unless", "b) if", "c) As long as"],
+        "correct_answer": 0
+    },
+    {
+        "question": "Вопрос 27: Rachel …… be in hospital; I just talked to her and she said she was at home.    ",
+        "options": ["a) mustn’t", "b) can’t", "c) won’t"],
+        "correct_answer": 1
+    },
+    {
+        "question": "Вопрос 28: Mary works …… a nurse at Pillby Hospital.",
+        "options": ["a) Such", "b) Like", "c) As"],
+        "correct_answer": 2
+    },
+    {
+        "question": "Вопрос 29:John is soaking wet because he …… in the rain.",
+        "options": ["a) has been walking", "b) will be walking", "c) had been walking"],
+        "correct_answer": 0
+    },
+    {
+        "question": "Вопрос 30: If you had studied harder, you …… the entrance exams.",
+        "options": ["a) will have passed", "b) would have passed", "c) will be passing"],
+        "correct_answer": 1
+    },
+    {
+        "question": "Вопрос 31: I wish I …… at Mike; he’s not talking to me now.",
+        "options": ["a) hadn’t shouted", "b) haven’t shouted", "c) didn’t shout"],
+        "correct_answer": 0
+    },
+    {
+        "question": "Вопрос 32: I don’t think there is …… bread left.",
+        "options": ["a) some", "b) Any", "c) No"],
+        "correct_answer": 1
+    },
+{
+        "question": "Вопрос 33: …… he is rich, he never spends any money.",
+        "options": ["a) despite", "b) although", "c) in side of"],
+        "correct_answer": 1
+    },
+    {
+        "question": "Вопрос 34: Do you remember …… that film? We went to the cinema together.",
+        "options": ["a) see", "b) to see", "c) seeing"],
+        "correct_answer": 2
+    },
+    {
+        "question": "Вопрос 35: The authorities …… rebuilding the houses.",
+        "options": ["a) are", "b) Are being", "c) been"],
+        "correct_answer": 0
+    },
+    {
+        "question": "Вопрос 36: Dogs are very loyal pets. ……, they can guard your property.",
+        "options": ["a) because", "b) what is More", "c) even though"],
+        "correct_answer": 1
+    },
+    {
+        "question": "Вопрос 37: It’s no use …… the hedges cut until next month.",
+        "options": ["a) have", "b) To have", "c) Having"],
+        "correct_answer": 2
+    },
+    {
+        "question": "Вопрос 38: Her hair is not …… Catherine’s.",
+        "options": ["a) so long", "b) As long as", "c) Long as"],
+        "correct_answer": 1
+    },
+    {
+        "question": "Вопрос 39: I’m not sure …… David lives.",
+        "options": ["a) Where", "b) In which", "c) that"],
+        "correct_answer": 0
+    },
+    {
+        "question": "Вопрос 40: You …… me!",
+        "options": ["a) criticise always", "b) are always criticising", "c) always are criticizing"],
+        "correct_answer": 1
+    },
+    {
+        "question": "Вопрос 41: Choose the correct option",
+        "options": ["a) When are you going to go out?", "b) When going out are we?", "c) When do we go out?"],
+        "correct_answer": 0
+    },
+    {
+        "question": "Вопрос 42: Choose the correct option",
+        "options": ["a) I work tomorrow", "b) I don't working tomorrow", "c)  I'm working tomorrow"],
+        "correct_answer": 2
+    },
+{
+        "question": "Вопрос 43: Choose the correct option",
+        "options": ["a) Did you finish your project?", "b) Have you finished your project?", "c) Have you got finished your project?"],
+        "correct_answer": 2
+    },
+    {
+        "question": "Вопрос 44: Choose the correct option",
+        "options": ["a) I am usually having some coffee and toast for my breakfast", "b)  I am used to have some coffee and toast for my breakfast", "c)  I usually have some coffee and toast for my breakfast"],
+        "correct_answer": 2
+    },
+{
+        "question": "Вопрос 45:Choose the correct option",
+        "options": ["a) I'm trying to eat a more healthy diet", "b) I try to eat a more healthy diet", "c)  I'm trying to eat a healthier diet"],
+        "correct_answer": 2
+    },
+    {
+        "question": "Вопрос 46: Choose the correct option",
+        "options": ["a) He's never been to New York", "b) He's never gone to New York", "c) He's gone often to New York"],
+        "correct_answer": 0
+    },
+    {
+        "question": "Вопрос 47: Choose the correct option",
+        "options": ["a) At this rate, they will never be here on time", "b) At this rate, they are never here on time", "c)  At this rate, they are never going here on time"],
+        "correct_answer": 0
+    },
+{
+        "question": "Вопрос 48: Choose the correct option",
+        "options": ["a) Are you studied Chinese before?", "b) Are you studying Chinese before?", "c) Have you studied Chinese before?"],
+        "correct_answer": 2
+    },
+    {
+        "question": "Вопрос 49: Choose the correct option",
+        "options": ["a) I can do that for you", "b)  I could do that", "c)  I could to make that for you"],
+        "correct_answer": 0
+    },
+    {
+        "question": "Вопрос 50: Choose the correct option",
+        "options": ["a) Are you going to University?", "b) Are you going to go to University?", "c) Do you like University?"],
+        "correct_answer": 1
+    },
+    {
+        "question": "Вопрос 51: Choose the correct option",
+        "options": ["a) You haven't to do that, you know", "b) You didn't have to do that, you know", "c)  You didn't must do that, you know"],
+        "correct_answer": 1
+    },
+{
+        "question": "Вопрос 52: Choose the correct option",
+        "options": ["a)  How long is it from Hong Kong to Shanghai?", "b) How far is it from Hong Kong to Shanghai?", "c) How much is it from Hong Kong to Shanghai?"],
+        "correct_answer": 1
+    },
+    {
         "question": "Вопрос 53: Choose the correct option",
         "options": ["a) When we finish the painting, we'll have a cup of tea", "b)  When we've finished the painting, we'll have a cup of tea", "c)  When the painting finishes, we'll have a cup of tea"],
         "correct_answer": 0
     },
-    {
+{
         "question": "Вопрос 54: He told her that...",
         "options": ["a)  He would love her forever", "b) He loved her forever", "c) He is loving her forever"],
         "correct_answer": 0
